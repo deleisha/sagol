51b5b6a4023f54b446d1521c68eaa2a179f63ad1
deleisha <dlmeetei@gmail.com>
2015-05-13 16:22:42 +0530

    Initial checkin of utils with queue

---
+++ src/utils/hash_table.h
@@ -0,0 +1,24 @@
+/*
+ Our own implementation of hash table
+*/
+
+
+
+#ifndef __HASH_TABLE_H_
+#define __HASH_TABLE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus 
+}
+#endif
+#endif
+
+
+
+
