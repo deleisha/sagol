3dddf77496e617ab7b2f52864a2cb932cfeead63
deleisha <dlmeetei@gmail.com>
2015-06-04 18:47:44 +0530

    made main program little suitable for presentation

--- main.c
+++ main.c
@@ -1,33 +1,28 @@
 #include "http_server.h"
 #include "connection.h"
 
-
 void write_res( const request *rqst, response *reply)
 {
-    char *str = "hello world\n";
-
+   char *str = "hello world\n";
    set_status(reply, 200);
    set_hdr(reply, "Content-Type",  "text/plain");
-
    reply->msg_len = strlen(str);
    reply->msg_body = str;
 }
 
-
-int main()
-{
-    //bring up tls machine, this need to be done before server setup
-    //if we are using tls
-    int ng = tls_engine_inhale("server-cert.pem", "server-key.pem", 0);
-    assert(ng == 0);
+int main() {
 
     const int port = 8000;
     http_server svc;
+
+    //bring up tls machine, this need to be done before server setup
+    int ng = tls_engine_inhale("server-cert.pem", "server-key.pem", 0);
+    assert(ng == 0);
+
     setup_server(&svc, "0.0.0.0", port);
 
+    //the length of path passed explicitly to stop strlen calling
     router *rtr = get_router(&svc);
-    //third arg is the length of path, passing this explicitly to stop
-    //strlen calling
     addroute(rtr, "/home", 5, write_res);
 
     printf("Listening on %d\n", port);
