2a7ec7fea660f2247ecb482a0bdcfcbe7fec81f3
deleisha <dlmeetei@gmail.com>
2015-06-09 19:18:24 +0530

    Redfish HTTP Status code complete list imported

--- src/http_status.h
+++ src/http_status.h
@@ -17,11 +17,26 @@ typedef struct dummy {
    ,XX(CREATED, {14, " 201 Created\r\n"} )                                    \
    ,XX(ACCEPTED, {16, " 202 Accepted\r\n"} )                                  \
    ,XX(NO_CONTENT,{20, " 204 No Content\r\n"} )                               \
+   ,XX(MOVED_PERMANENTLY, {24, " 301 Moved Permanently\r\n"} )                \
+   ,XX(FOUND, {12, " 302 Found\r\n"} )                                        \
+   ,XX(NOT_MODIFIED, {19, " 304 Not Modified\r\n"} )                          \
+   ,XX(BAD_REQUEST, {18, " 400 Bad Request\r\n"} )                            \
+   ,XX(UNAUTHORIZED, {19, " 401 Unauthorized\r\n"} )                          \
+   ,XX(FORBIDDEN, {16, " 403 Forbidden\r\n"} )                                \
+   ,XX(NOT_FOUND, {16, " 404 Not Found\r\n"} )                                \
+   ,XX(MET_NOT_ALLOWED, {25, " 405 Method Not Allowed\r\n"} )                 \
+   ,XX(NOT_ACCEPTABLE, {21, " 406 Not Acceptable\r\n"} )                      \
+   ,XX(GONE, {11, " 410 Gone\r\n"} )                                          \
+   ,XX(LEN_REQD, {22, " 411 Length Required\r\n"} )                           \
+   ,XX(PRECOND_FAILED, {26, " 412 Precondition Failed\r\n"} )                 \
+   ,XX(UNSUPPORTED_MIME_TYPE, {29, " 415 Unsupported Media Type\r\n"} )       \
+   ,XX(INTERNAL_ERROR, {26, " 500 Internal Server Error\r\n"} )               \
+   ,XX(NOT_IMPLEMENTED, {22, " 501 Not Implemented\r\n"} )                    \
+   ,XX(SVC_UNAVAILABLE, {26, " 503 Service Unavailable\r\n"} )                \
 
 
 
 
-#define XX(code, len, wrd) len, wrd
 
 /* http_status_info array[] = {
     {9, "200 Ok\r\n"},
@@ -30,6 +45,7 @@ typedef struct dummy {
 }; */
 
 static http_status_info array[] = {
+#define XX(code, len, wrd) len, wrd
     HTTP_STATUS_MAP(XX)
 #undef XX
 };
@@ -42,7 +58,6 @@ static http_status_info array[] = {
 #define GET_HTTP_STATUS_STR(code, len)    (len = array[code].sz, array[code].msg)
 
 
-#define XX(code, l, word) HTTP_STATUS_##code
 
 /* enum http_status {
     HTTP_STATUS_OK,
@@ -50,8 +65,10 @@ static http_status_info array[] = {
 }; */
 
 enum http_status {
+#define XX(code, l, word) HTTP_STATUS_##code
     HTTP_STATUS_MAP(XX)
 #undef XX
+   ,MAX_VALUE
 };
 
 
